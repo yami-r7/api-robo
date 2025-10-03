453240871e3b661884310ad4d4ce88dc6590c9ea
yami-r7 <rafaelyami7@hotmail.com>
2025-10-03 11:01:57 -0300

    Fix: CorreÃigindo URL no OpenApiConfig

--- src/main/java/br/com/senai/robo/config/OpenApiConfig.java
+++ src/main/java/br/com/senai/robo/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package br.com.senai.robo.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://api-robo-production.up.railway.app"))
                 .info(new Info()
                         .title("API Robô SENAI")
                         .version("v1")

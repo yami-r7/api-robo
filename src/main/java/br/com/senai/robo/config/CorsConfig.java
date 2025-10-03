bd78a6d8febf32c3ca7b9df9f524f0cfc41d9fdd
yami-r7 <rafaelyami7@hotmail.com>
2025-10-03 11:55:04 -0300

    Refactor: ApAdiciona novo link ao CorsConfig

--- src/main/java/br/com/senai/robo/config/CorsConfig.java
+++ src/main/java/br/com/senai/robo/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:3000",
                         "http://127.0.0.1:5500",
-                        "https://api-robo-production.up.railway.app"
+                        "https://api-robo-production.up.railway.app",
+                        "http://localhost:8081"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"); // Métodos HTTP permitidos
     }
